9f23d3a7e2d6119470a503269339a1231f53a130
Abraham123-dev <abrahamoluwaniyi50@gmail.comgit config --global user.name Abraham.Dev>
2026-03-20 17:33:41 +0100

    commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import { clsx } from 'clsx';
 
 const Header = () => {
-  const [scrolled, setScrolled] = useState(false);
+  const [scrolled, setScrolled] = useState(True);
 
   useEffect(() => {
     const handleScroll = () => {
